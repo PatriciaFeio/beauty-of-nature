412b160047e66b34b73602118be95ef779adcb9c
Patricia Feio <patriciafeio@gmail.com>
2020-01-29 00:18:50 +0000

    Added JavaScript to control background image slider

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -3,3 +3,35 @@ import '../styles/styles.css';
 if (module.hot) {
   module.hot.accept();
 }
+
+let sliderImages = document.querySelectorAll('.slider-backg__slide'),
+  arrowForward = document.querySelector('.arrow-slider'),
+  current = 0;
+
+// clear all images
+function reset() {
+  for (let i = 0; i < sliderImages.length; i++) {
+    sliderImages[i].style.display = 'none';
+  }
+}
+
+//init slider
+function startSlide() {
+  reset();
+  sliderImages[0].style.display = 'block';
+}
+
+//show next
+function slideForward() {
+  reset();
+  sliderImages[current + 1].style.display = 'block';
+  current++;
+}
+
+//arrow click
+arrowForward.addEventListener('click', function() {
+  if (current === sliderImages.length - 1) {
+    current = -1;
+  }
+  slideForward();
+});
